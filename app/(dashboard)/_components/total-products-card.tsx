85fb0537015c07f58b36caa220110fce3249253a
marcoss741-0 <marcossn741@icloud.com>
2025-04-05 17:48:21 -0300

    atualiza tags de cache para o total de produtos no dashboard

--- app/(dashboard)/_components/total-products-card.tsx
+++ app/(dashboard)/_components/total-products-card.tsx
@@ -5,9 +5,11 @@ import SummaryCards, {
   SummaryCardValue,
 } from "./summary-cards";
 import cachedGetTotalProducts from "@/app/data-access/dashboard/get-total-products";
+import { revalidateTag } from "next/cache";
 
 const TotalProductsCard = async () => {
   const totalProducts = await cachedGetTotalProducts();
+  revalidateTag("get-total-products");
   return (
     <>
       <SummaryCards>
